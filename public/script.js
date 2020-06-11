c620b46eca12b9a20521c45bd121b7546197ca11
Page Nathan (DC-MH/ENG1.13-NA) <nathan.page@bosch.com>
2020-06-11 17:24:34 -0400

    Need to fix layout in index.html and button function in scrpit.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 
 
     $('.btn-start').click(function() {
-      
+      alert("Start button pressed")
       $.ajax({
         type: "POST",
         url: "http://127.0.0.1:3002/DataDashboard",
         data: "\Start",
-        contentType: "text/JSON",
+        contentType: "text/plain",
         dataType: "json",
         success: function(data) {
           //$('.footer-text').text('Hello, World!')
