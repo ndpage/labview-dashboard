d906675d9b6e7c51a37184927fbae97d4cc16eda
Page Nathan (DC-MH/ENG1.13-NA) <nathan.page@bosch.com>
2020-06-11 17:13:40 -0400

    Basic server to serve everying thing in the public folder

---
+++ server.js
@@ -0,0 +1,19 @@
+
+// Package declarations for this server.js file
+
+var express= require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var port = 3000;
+var hostname = '127.0.0.1';
+// Configuation for the server app
+
+app.use(express.static(__dirname + '/public')); // Serve everything in the public directory
+app.use(bodyParser.json())
+  
+app.get('/', (req, res) => res.sendFile('public/index.html'))
+
+
+// Start the server
+app.listen(port, hostname); //127.0.0.1
+console.log("Listening at http://" + hostname + ":" + port);
